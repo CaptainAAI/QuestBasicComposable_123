8dde0cef669cb758007b9ff7cfb818d4440b056b
Ananda Agung Ismail <158981471+CaptainAAI@users.noreply.github.com>
2025-07-30 08:05:52 +0000

    skema warna

--- app/src/main/java/com/example/activity2/ui/theme/Theme.kt
+++ app/src/main/java/com/example/activity2/ui/theme/Theme.kt
@@ -28,6 +28,16 @@ fun Activity2Theme(
 )
 {
 
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context)
+            else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
 }
 
 
